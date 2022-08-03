7322af313e4316711199894fbfb6a98a7e41afae
EdersonDeMelo <drsnmelo@gmail.com>
2022-08-03 08:13:40 -0300

    Adicionado repositorio do gateway

---
+++ mscloudgateway/src/test/java/com/ederson/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederson.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
