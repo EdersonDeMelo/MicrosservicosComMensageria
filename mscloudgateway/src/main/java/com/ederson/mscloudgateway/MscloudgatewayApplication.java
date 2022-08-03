ea8e22007a84a3403dfa784ebd4e3444fb291f98
EdersonDeMelo <drsnmelo@gmail.com>
2022-08-03 08:26:10 -0300

    Configurado application

--- mscloudgateway/src/main/java/com/ederson/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/com/ederson/mscloudgateway/MscloudgatewayApplication.java
@@ -3,6 +3,8 @@ package com.ederson.mscloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
@@ -12,6 +14,13 @@ public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	public RouteLocator routes (RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/clientes?**").uri("lb://msclientes"))
+				.build();
 	}
 
 }
